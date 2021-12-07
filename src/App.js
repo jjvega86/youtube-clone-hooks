82d1d7e0b874ca987a813855a3d7a8a85369e2b3
JJ <jj.vega86@gmail.com>
2021-12-07 05:58:52 -0500

    Added hidden apiKey and initial search fetching logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { apiKey } from "./apiKey";
 
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 const App = () => {
-  const [currentVideo, setCurrentVideo] = useState("M7lc1UVf-VE");
+  const [currentVideo, setCurrentVideo] = useState({
+    id: {
+      videoId: "Oa0pMn0tvU4",
+    },
+    snippet: {
+      title: "LOADING",
+      description: "LOADING",
+    },
+  });
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetchYouTubeVideos("starwars");
+  }, []);
 
-  const fetchYouTubeVideos = (searchTerm) => {
+  const fetchYouTubeVideos = async (searchTerm) => {
     try {
-    } catch (error) {}
+      let response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=${apiKey}&part=snippet&type=video&maxResults=5`
+      );
+      setCurrentVideo(response.data.items[0]);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   return (
     <div>
-      <VideoPlayer videoId={currentVideo} />
+      <VideoPlayer
+        videoId={currentVideo.id.videoId}
+        title={currentVideo.snippet.title}
+        description={currentVideo.snippet.description}
+      />
     </div>
   );
 };
